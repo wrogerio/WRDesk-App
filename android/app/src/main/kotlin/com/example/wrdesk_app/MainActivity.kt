ba9c602fd2e643e717ac506029121c7048d775b9
Wellington Rogério <wrogerio.ing@gmail.com>
2022-11-17 22:51:24 -0300

    initial

---
+++ android/app/src/main/kotlin/com/example/wrdesk_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wrdesk_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:wrdesk_app/pages/home.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
+}
